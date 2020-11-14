78edeca3c9288426d339b97fc69e90c3ad4d6612
kawseribn <46264704+kawseribn@users.noreply.github.com>
2020-11-15 03:50:29 +0600

    Update index.py

--- api/index.py
+++ api/index.py
@@ -5,11 +5,11 @@ import json
 class handler(BaseHTTPRequestHandler):
 
     def do_GET(self):
-        self.output= {'male':'আবরেশমী শাহেদ'}
-        self.json1=json.dumps(self.output)
+        output= {'male':'আবরেশমী শাহেদ'}
+        json1=json.dumps(output)
         self.send_response(200)
         self.send_header('Content-type','text/plain')
         self.end_headers()
-        message = json.load(self.json1)
+        message = json.load(json1)
         self.wfile.write(message.encode())
         return
