196fa0a0399600196e98917444c04a00120a0f88
kawseribn <46264704+kawseribn@users.noreply.github.com>
2020-11-15 04:08:19 +0600

    Update index.py

--- api/index.py
+++ api/index.py
@@ -11,5 +11,5 @@ class handler(BaseHTTPRequestHandler):
         self.send_header('Content-type','text/plain')
         self.end_headers()
         message = json1
-        self.wfile.write(message.encode())
+        self.wfile.write(message.encode("utf-8"))
         return
