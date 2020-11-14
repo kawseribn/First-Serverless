71331073758893ef749131a58e7fcbac726498f1
kawseribn <kawser.ibn.93@gmail.com>
2020-11-15 03:34:17 +0600

    adding json

--- api/index.py
+++ api/index.py
@@ -1,12 +1,15 @@
 from http.server import BaseHTTPRequestHandler
 from cowpy import cow
+import json
 
 class handler(BaseHTTPRequestHandler):
 
     def do_GET(self):
+        self.output= {'male':'আবরেশমী শাহেদ'}
+        self.json1=json.dumps(output)
         self.send_response(200)
         self.send_header('Content-type','text/plain')
         self.end_headers()
-        message = cow.Cowacter().milk('Hello from Python from a Serverless Function!')
+        message = json1
         self.wfile.write(message.encode())
         return
