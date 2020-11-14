474cc1f546b1256332c956b86c0baf101182b342
kawseribn <46264704+kawseribn@users.noreply.github.com>
2020-11-15 05:03:07 +0600

    Update index.py

--- api/index.py
+++ api/index.py
@@ -7,7 +7,7 @@ class handler(BaseHTTPRequestHandler):
         self.send_response(200)
         self.send_header('Content-type','text/plain')
         self.end_headers()
-        output= {'female':'আবরেশমী শাহেদ'}
+        output= {'female':'kawser'}
         message=json.dumps(output,ensure_ascii=False)
-        self.write(message)
+        self.wfile.write(message.encode())
         return
