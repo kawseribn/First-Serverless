f1be036d17807fa3e8ef5a88ac306295106794d1
kawseribn <46264704+kawseribn@users.noreply.github.com>
2020-11-15 05:54:46 +0600

    Update index.py

--- api/index.py
+++ api/index.py
@@ -7,6 +7,7 @@ class handler(BaseHTTPRequestHandler):
         self.send_header('Content-type','text/plain; charset=utf-8')
         self.end_headers()
         output= {'female': 'কনিকা সাদিয়া'}
-        message=json.loads(message)
+        json1=json.dumps(output,ensure_ascii=False)
+        message=json.loads(json1)
         self.wfile.write(message['female'].encode('utf-8'))
         return
